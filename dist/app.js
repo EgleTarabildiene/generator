b1535e826025e3d6d206fa1f4a9cb56594203e03
Egle <157697524+EgleTarabildiene@users.noreply.github.com>
2024-05-31 22:56:02 +0300

    add delete

--- dist/app.js
+++ dist/app.js
@@ -12,8 +12,7 @@ const con = mysql2_1.default.createConnection({
     host: "localhost",
     user: "root",
     password: "LabasRytas",
-    port: 3306,
-    database: "employees"
+    database: "company"
 });
 con.connect((error) => {
     if (error)
@@ -21,7 +20,6 @@ con.connect((error) => {
     connected = true;
     console.log("Prisijungta");
 });
-//Sukuriame http serverį ir paduodame f-ją kuri bus vykdoma kai ateis užklausa
 const server = http_1.default.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
@@ -45,7 +43,6 @@ const server = http_1.default.createServer((req, res) => {
         res.write(file);
         return res.end();
     }
-    //Jei kažkas atėjo į puslapį su GET metodu į url: localhost:2999/employees, išsiunčiame jam employeeų sąrašą HTML formatu
     if (url == '/employees' && method == 'GET') {
         if (connected) {
             con.query("SELECT * FROM employees ORDER BY name ASC;", (error, result) => {
@@ -55,7 +52,7 @@ const server = http_1.default.createServer((req, res) => {
                 let rows = "";
                 result.forEach((e) => {
                     rows += "<tr>";
-                    rows += `<td>${e.name}</td> <td>${e.surname}</td> <td>${e.phone}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows += `<td>${e.name}</td> <td>${e.surname}</td> <td>${e.phone}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a> <a href='/delete/${e.id}' class="btn btn-danger">Ištrinti</a></td>`;
                     rows += "</tr>";
                 });
                 let template = fs_1.default.readFileSync('templates/employees.html').toString();
@@ -65,10 +62,44 @@ const server = http_1.default.createServer((req, res) => {
             });
         }
     }
-    //Vieno employee atvaizdavimas, kai url = localhost:2999/employee/5
+    if (url == '/add' && method == 'POST') {
+        if (connected) {
+            const reqBody = [];
+            req.on('data', (d) => {
+                reqBody.push(d);
+            });
+            req.on('end', () => {
+                const reqData = decodeURIComponent(Buffer.concat(reqBody).toString());
+                const dd = reqData.split('&');
+                console.log(dd);
+                const name = mysql2_1.default.escape(dd[0].split('=')[1]);
+                const surname = mysql2_1.default.escape(dd[1].split('=')[1]);
+                const gender = mysql2_1.default.escape(dd[2].split('=')[1]);
+                const phone = mysql2_1.default.escape(dd[3].split('=')[1]);
+                const birthday = mysql2_1.default.escape(dd[4].split('=')[1]);
+                const education = mysql2_1.default.escape(dd[5].split('=')[1]);
+                const salary = mysql2_1.default.escape(dd[6].split('=')[1]);
+                const sql = `INSERT INTO employees(name, surname, gender, phone, birthday, education, salary) VALUES (${name}, ${surname}, ${gender}, ${phone}, ${birthday}, ${education},${salary})`;
+                con.query(sql, (error) => {
+                    if (error)
+                        throw error;
+                });
+                res.writeHead(302, {
+                    'Location': '/employees'
+                });
+                res.end();
+            });
+        }
+    }
+    if (url == '/add' && method == 'GET') {
+        if (connected) {
+            let template = fs_1.default.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
+        }
+    }
     console.log(url === null || url === void 0 ? void 0 : url.split("/"));
     if ((url === null || url === void 0 ? void 0 : url.split("/")[1]) == 'employee') {
-        //Pasiimame iš url id
         let id = parseInt(url === null || url === void 0 ? void 0 : url.split("/")[2]);
         con.query(`SELECT * FROM employees WHERE id=${id};`, (error, result) => {
             if (error)
@@ -78,12 +109,24 @@ const server = http_1.default.createServer((req, res) => {
             let template = fs_1.default.readFileSync('templates/employee.html').toString();
             template = template.replace("{{ name }}", employee.name);
             template = template.replace("{{ surname }}", employee.surname);
-            template = template.replace("{{ gender }}", employee.sex != null ? employee.sex : '-');
+            template = template.replace("{{ gender }}", employee.gender != null ? employee.gender : '-');
             template = template.replace("{{ phone }}", employee.phone != null ? employee.phone : '-');
             template = template.replace("{{ birthday }}", employee.birthday != null ? employee.birthday.toLocaleDateString() : '-');
             template = template.replace("{{ education }}", employee.education != null ? employee.education : '-');
             template = template.replace("{{ salary }}", employee.salary != null ? employee.salary : '-');
             res.write(template);
+            res.end();
+        });
+    }
+    if ((url === null || url === void 0 ? void 0 : url.split("/")[1]) == 'delete') {
+        //Pasiimame iš url id
+        let id = parseInt(url === null || url === void 0 ? void 0 : url.split("/")[2]);
+        con.query(`DELETE FROM employees WHERE id=${id};`, (error, result) => {
+            if (error)
+                throw error;
+            res.writeHead(302, {
+                'Location': '/employees'
+            });
             res.end();
         });
     }
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Employee } from './models/employee';
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import { buffer } from 'stream/consumers';
 
 
 let connected=false;
@@ -12,8 +13,8 @@ const con=mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"LabasRytas",
-    port: 3306,
-    database:"employees"
+    
+    database:"company"
 
 });
 
@@ -34,6 +35,11 @@ const server=http.createServer((req, res)=>{
     const url=req.url;
     const method=req.method;
     
+
+
+
+
+
 
  
     let filePath=`public${url}`;
@@ -68,18 +74,70 @@ const server=http.createServer((req, res)=>{
                 let rows="";
                 result.forEach((e)=>{ 
                     rows+="<tr>";
-                    rows+=`<td>${e.name}</td> <td>${e.surname}</td> <td>${e.phone}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows+=`<td>${e.name}</td> <td>${e.surname}</td> <td>${e.phone}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a> <a href='/delete/${e.id}' class="btn btn-danger">Ištrinti</a></td>`;
                     rows+="</tr>";
                 });
                
                 let template=fs.readFileSync('templates/employees.html').toString();
                 template=template.replace('{{ employees_table }}', rows);
               
+
                 res.write(template);        
                 res.end();
             });   
         }
     }
+
+
+    if(url=='/add' && method=='POST'){
+        if(connected){
+     const reqBody:any[]=[];
+     req.on('data', (d)=>{
+        reqBody.push(d);
+     });
+     req.on('end', ()=>{
+        const reqData=decodeURIComponent(Buffer.concat(reqBody).toString());
+        const dd=reqData.split('&');
+        console.log(dd);
+        const name = mysql.escape(dd[0].split('=')[1]);
+        const surname= mysql.escape(dd[1].split('=')[1]);
+        const gender= mysql.escape(dd[2].split('=')[1]);
+        const phone= mysql.escape(dd[3].split('=')[1]);
+        const birthday= mysql.escape(dd[4].split('=')[1]);
+        const education= mysql.escape(dd[5].split('=')[1]);
+        const salary= mysql.escape(dd[6].split('=')[1]);
+
+const sql=`INSERT INTO employees(name, surname, gender, phone, birthday, education, salary) VALUES (${name}, ${surname}, ${gender}, ${phone}, ${birthday}, ${education},${salary})`;
+con.query(sql, (error)=>{
+    if (error) throw error;
+});
+
+
+        res.writeHead(302, {
+            'Location':'/employees'
+        });
+        res.end();
+        });
+    }
+}
+
+
+
+
+    if(url=='/add' && method=='GET'){
+        if(connected){
+            let template=fs.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
+        }
+    }
+
+
+
+
+
+
+
 
    
     console.log(url?.split("/"));
@@ -94,9 +152,9 @@ const server=http.createServer((req, res)=>{
             let template=fs.readFileSync('templates/employee.html').toString();
             template=template.replace("{{ name }}", employee.name);
             template=template.replace("{{ surname }}", employee.surname);
-            template=template.replace("{{ gender }}", employee.sex!=null?employee.sex:'-');
+            template=template.replace("{{ gender }}", employee.gender!=null?employee.gender:'-');
             template=template.replace("{{ phone }}", employee.phone!=null?employee.phone:'-');
-           template=template.replace("{{ birthday }}", employee.birthday!=null?employee.birthday.toLocaleDateString():'-');
+            template=template.replace("{{ birthday }}", employee.birthday!=null?employee.birthday.toLocaleDateString():'-');
             template=template.replace("{{ education }}", employee.education!=null?employee.education:'-');
             template=template.replace("{{ salary }}", employee.salary!=null?employee.salary:'-');
             
@@ -108,6 +166,26 @@ const server=http.createServer((req, res)=>{
         
         
     }
+
+    if ( url?.split("/")[1] == 'delete' ){
+        //Pasiimame iš url id
+        let id=parseInt(url?.split("/")[2]);
+        con.query<Employee[]>(`DELETE FROM employees WHERE id=${id};`, (error,result)=>{
+            if (error) throw error;
+            res.writeHead(302, {
+                'Location':'/employees'
+            })
+            res.end();
+        });
+        
+        
+    }
+
+
+
+
+
+
 
     
 
